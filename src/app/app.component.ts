a3aeb8612b8a8e844322388f31f00be4f2552d95
Amro Abdrabo <amro.abdrabo@gmail.com>
2026-05-07 16:04:46 +0200

    Scaffold blank Angular application

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: '',
+  styles: [
+    ':host { display: block; min-height: 100vh; }'
+  ]
+})
+export class AppComponent {}
